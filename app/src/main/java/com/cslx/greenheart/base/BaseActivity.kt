db08e434d8a0ff41f3e29bbc45a8736f65bdddd3
jiangyaheng <jyhryn@163.com>
2020-12-16 17:32:18 +0800

    1

--- app/src/main/java/com/cslx/greenheart/base/BaseActivity.kt
+++ app/src/main/java/com/cslx/greenheart/base/BaseActivity.kt
@@ -1,6 +1,10 @@
 package com.cslx.greenheart.base
 
 import android.app.Activity
+import android.os.Bundle
 
 class BaseActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
