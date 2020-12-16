d0e0b14d5c484a301fc4744eb2aa88ee2f5bfa72
jiangyaheng <jyhryn@163.com>
2020-12-16 16:42:26 +0800

    首次提交

---
+++ app/src/main/java/com/cslx/greenheart/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.cslx.greenheart
+
+import android.app.Application
+
+class MyApplication : Application{
+    constructor() : super()
+
+
+}
---
+++ app/src/main/java/com/cslx/greenheart/base/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.cslx.greenheart.base
+
+import android.app.Activity
+
+class BaseActivity : Activity(){
+}
